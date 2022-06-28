823ec4fb1ddcb1e83de23fae802b7ea07c7f9227
xyzjesus <xyzjesus@LAPTOP-PL02A6PJ>
2022-06-28 15:50:00 +0400

    initial coomit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { initializeContract } from "./components/utils/near";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import "bootstrap";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+const container = document.getElementById("root")
+const root = createRoot(container);
+
+window.nearInitPromise = initializeContract()
+  .then(() => {
+    root.render(
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>,
+    );
+  })
+  .catch(console.error);
+
 reportWebVitals();
